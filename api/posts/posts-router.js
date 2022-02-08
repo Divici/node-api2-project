e8bead0015a2fe425499803cedef2e9e50030c13
david-aihe <doa9200@gmail.com>
2022-02-08 09:32:30 -0500

    get posts and post by id requests handled

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,14 +1,29 @@
 // implement your posts router here
 const express = require('express')
+const PostModel = require('./posts-model');
 
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.json('testing get');
+    PostModel.find()
+        .then((posts) => {
+            res.json(posts)
+        }).catch((err) => {
+            res.status(500).json({message: "The posts information could not be retrieved"})
+        });
 })
 
-router.get('/:id', (req, res) =>{
-
+router.get('/:id', async (req, res) =>{
+    try {
+        const post = await PostModel.findById(req.params.id);
+        if(!post){
+            res.status(404).json({message: "The post with the specified ID does not exist"})  
+        }
+        res.json(post);
+    }
+    catch (err) {
+        res.status(500).json({message: "The post information could not be retrieved"})  
+    }
 })
 
 router.post('/', (req, res) =>{
